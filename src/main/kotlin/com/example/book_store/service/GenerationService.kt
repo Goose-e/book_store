8f3b2c2a60113d02c7ac65c3edba4934141d1b8a
Goose <Kolosovich19@gmail.com>
2024-05-22 09:45:03 +0300

    c

--- src/main/kotlin/com/example/book_store/service/GenerationService.kt
+++ src/main/kotlin/com/example/book_store/service/GenerationService.kt
@@ -8,6 +8,7 @@ import kotlin.random.Random
 @Service
 class GenerationService {
     companion object {
+
         fun generateEntityId() = (now().toEpochSecond(ZoneOffset.UTC).toString()).toLong()+Random.nextLong(10000)
         fun generateCode() = Random.nextInt(100, 100000).toString() + Random.nextBits(10)
     }
